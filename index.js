360d899d58931de3102e657ba105e502aac73be5
Wilson Kimunya <kimunya.wilson@gmail.com>
2023-07-04 16:48:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const RightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(RightNumbers, 10);
+  
+    if (right <180) {
+      dodger.style.left = `${right + 1}px`;
+    }
+  }
